4cac599a9056f4b94a65a8ce687f725d1ae68e77
Miguel Rust <miguel@Miguels-Mac-mini.local>
2021-11-17 23:13:21 -0700

    Integrated everything

--- astar.py
+++ astar.py
@@ -1,8 +1,11 @@
 import nle
 import gym
 
+from abc import ABC, abstractmethod
 from heapq import heappop, heappush
 from main import MoveActions, Space
+from Agent import Agent
+from sys import argv
 
 
 
@@ -44,8 +47,39 @@ class Node:
     def __repr__(self) -> str:
         return str(self.__position)
 
+class PathFindingAgent(ABC):
+    """Abstract class for agents A* is compatible with"""
 
-def findPathInGridWorld(map: list, start: tuple, goal: tuple):
+    @abstractmethod
+    def isDoor(self, y, x) -> bool:
+        """Returns true if grid cell is door"""
+
+    @abstractmethod 
+    def isNotWall(self, y, x, diagonal=False) -> bool:
+        """Returns true if cell not wall"""
+
+    @abstractmethod
+    def isWall(self, y, x, diagonal=False) -> bool:
+        """Returns true if cell is wall"""
+    
+    @abstractmethod
+    def getPossibleMoves(self, y, x) -> tuple:
+        """Returns possible moves first element action number, second element coordinate"""
+
+    @abstractmethod
+    def getEnviromentDimensions(self) -> tuple:
+        """Returns dimensions of enviroment"""
+
+    @abstractmethod
+    def getX(self) -> int:
+        """Gets x position of agent"""
+
+    @abstractmethod 
+    def getY(self) -> int:
+        """Gets y position of agent"""
+
+
+def findPathInGridWorld(agent: PathFindingAgent, start: tuple, goal: tuple):
     """A* algorithm for nle grid world"""
     actionDirection = {
         MoveActions.UP: (0, -1),
@@ -57,10 +91,10 @@ def findPathInGridWorld(map: list, start: tuple, goal: tuple):
         MoveActions.DOWN_RIGHT: (1, 1),
         MoveActions.UP_LEFT: (-1, -1)
     }
-    cannotMove = set((Space.HORIZONTAL_WALL.value, Space.VERTICAL_WALL.value, Space.EMPTYNESS.value))
     openSet = {start: Node(start, 0, None)}
     openQueue = [openSet[k] for k in openSet]
     closedSet = {}
+    height, width = agent.getEnviromentDimensions()
     while len(openSet) > 0:
         current = heappop(openQueue)
         openSet.pop(current.getPosition())
@@ -76,8 +110,8 @@ def findPathInGridWorld(map: list, start: tuple, goal: tuple):
                     current = current.getParent()
                 return list(reversed(path))
             if not (newX, newY) in closedSet:
-                inBounds = newY < len(map) and newX < len(map[0]) and newX >= 0 and newY >= 0
-                if inBounds and not map[newY][newX] in cannotMove:
+                inBounds = newY < height and newX < width and newX >= 0 and newY >= 0
+                if inBounds and agent.isNotWall(newY, newX):
                     if (newX, newY) in openSet:
                         openSet[(newX, newY)].updateCost(current.getCost() + 1, current)
                     else:
@@ -98,6 +132,44 @@ def renderTestData(data: list, path: set = set()) -> None:
                 print(' ', end='')
         print()
 
+
+
+class MockAgent(PathFindingAgent):
+    """Mocks the agent for testing purposes"""
+    __cannotMove = set((Space.HORIZONTAL_WALL.value, Space.VERTICAL_WALL.value, Space.EMPTYNESS.value))
+    __glyphs: list = None
+
+    def __init__(self, glyphs: list) -> None:
+        self.__glyphs = glyphs
+
+    def isDoor(self, y, x) -> bool:
+        """There are no doors in mock data yet"""
+        return False 
+
+    def isNotWall(self, y, x, diagonal=False) -> bool:
+        return not self.__glyphs[y][x] in self.__cannotMove
+
+    def isWall(self, y, x, diagonal=False) -> bool:
+        return self.__glyphs[y][x] in self.__cannotMove
+
+    def getPossibleMoves(self, y, x) -> tuple:
+        """A* algo is not using actions only coords"""
+        coords = []
+        for row in range(max(y-1, 0), min(y+1, len(self.__glyphs))):
+            for col in range(max(x-1, 0), min(x+1, len(self.__glyphs[0]))):
+                if self.isNotWall(y, x) and (row, col) != (y, x):
+                    coords.append((y, x))
+        return [], coords
+
+    def getEnviromentDimensions(self) -> tuple:
+        return (len(self.__glyphs), len(self.__glyphs[0]))
+
+    def getY(self) -> int:
+        return 0
+
+    def getX(self) -> int:
+        return 0
+
 if __name__ == "__main__":
     #env = gym.make("NetHackScore-v0")
     #env.reset()
@@ -107,40 +179,48 @@ if __name__ == "__main__":
     #print((x, y))
     #print(findPathInGridWorld(state['glyphs'], (x,y), (x+5, y+5)))
     
-    # Tests for A* 
-    testData1 = [
-        [Space.HORIZONTAL_WALL.value for _ in range(10)],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(4)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.HORIZONTAL_WALL.value for _ in range(10)],
-    ]
-        
-    renderTestData(testData1, set(findPathInGridWorld(testData1, (1, 8), (8, 8))))
-    renderTestData(testData1, set(findPathInGridWorld(testData1, (1, 1), (8, 8))))
-    renderTestData(testData1, set(findPathInGridWorld(testData1, (2, 5), (8, 8))))
-
-    testData2 = [
-        [Space.HORIZONTAL_WALL.value for _ in range(10)],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, Space.VERTICAL_WALL.value] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
-        [Space.HORIZONTAL_WALL.value for _ in range(10)],
-    ]
-
-    renderTestData(testData2, set(findPathInGridWorld(testData2, (1, 8), (8, 8))))
-    renderTestData(testData2, set(findPathInGridWorld(testData2, (1, 1), (8, 8))))
-    renderTestData(testData2, set(findPathInGridWorld(testData2, (2, 5), (8, 8))))
     
+    if '-t' in argv:
+        # Tests for A* will run tests if -t argument provided
+        
+        testData1 = [
+            [Space.HORIZONTAL_WALL.value for _ in range(10)],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(4)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.HORIZONTAL_WALL.value for _ in range(10)],
+        ]
+        mockOne = MockAgent(testData1)   
+        renderTestData(testData1, set(findPathInGridWorld(mockOne, (1, 8), (8, 8))))
+        renderTestData(testData1, set(findPathInGridWorld(mockOne, (1, 1), (8, 8))))
+        renderTestData(testData1, set(findPathInGridWorld(mockOne, (2, 5), (8, 8))))
+
+        testData2 = [
+            [Space.HORIZONTAL_WALL.value for _ in range(10)],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(8)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, 0] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(3)] + [Space.VERTICAL_WALL.value] + [0, Space.VERTICAL_WALL.value] + [Space.VERTICAL_WALL.value] + [0] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.VERTICAL_WALL.value] + [0 for _ in range(5)] + [Space.VERTICAL_WALL.value] + [0 for _ in range(2)] + [Space.VERTICAL_WALL.value],
+            [Space.HORIZONTAL_WALL.value for _ in range(10)],
+        ]
+        mockTwo = MockAgent(testData2)  
+
+        renderTestData(testData2, set(findPathInGridWorld(mockTwo, (1, 8), (8, 8))))
+        renderTestData(testData2, set(findPathInGridWorld(mockTwo, (1, 1), (8, 8))))
+        renderTestData(testData2, set(findPathInGridWorld(mockTwo, (2, 5), (8, 8))))
+    else:
+        # If test flag not provided run with actual agent
+        agent = Agent("NetHackScore-v0")
+        print(findPathInGridWorld(agent, (agent.getX(), agent.getY()), (8, 8)))
+        agent.render()
